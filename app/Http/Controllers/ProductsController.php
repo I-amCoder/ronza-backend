d529d71b2a473311220bb7e2710ae2a99aa67188
Junaid Ali <Junaid Ali>
2023-06-18 17:09:30 +0500

    Hi

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -21,18 +21,6 @@ class ProductsController extends Controller
      */
     public function index()
     {
-        $products = Product::all();
-        foreach($products as $p){
-            $slug = $this->generateUniqueSlug($p->title, Product::class);
-            $p->slug = $slug;
-            $p->save();
-        }
-        $products = Category::all();
-        foreach($products as $p){
-            $slug = $this->generateUniqueSlug($p->name, Category::class);
-            $p->slug = $slug;
-            $p->save();
-        }
         $products = Product::paginate(15);
         return view('products.index', compact('products'));
     }
